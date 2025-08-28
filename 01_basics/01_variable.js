0451303c43b7777a8d2256ade7c3905ab55f4b52
nishant kumar <143006418+NISHANTKUMAR2004@users.noreply.github.com>
2025-08-28 04:29:05 +0000

    variables done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const a=1234
+let em="nishant@gmail.com"
+var v="233"
+console.log(a);
+console.log(em);
+console.log(v);
+
+console.table([a,em,v])
+function fibonacci(Number n){
+    // if(n<=1) return n;
+    // let a=0,b=1fvfv
+    let f=1
+    for(let i=1;i<=n;i++){
+        f*=i;
+    }
+    return f
+}
+console.log(fibonacci(10));
+console.log("hii there!")
+console.log("hii there!")
+
+
